ad36ee9c01e974f71e6e8bd02b816c8b37a88892
pverkind <peter.verkinderen@gmail.com>
2024-01-11 10:40:31 +0100

    Update website

--- README.md
+++ README.md
@@ -3,17 +3,27 @@
 This repository contains the silk road map quizzes from the Silk Road Seattle project
 (made in 2002 by Lance Jenott).
 
-The javascript does not work very well with modern browsers, so I wrote a converter to 
-create more modern versions of the quizzes, with a few additional features. 
+The javascript does not work very well with modern browsers, so I wrote a converter (convert.py) to 
+create more modern versions of the quizzes, with a few additional features:
+* feedback that includes the number of skipped places
+* a "show" button that shows the location of a place and then skips to the next place
+* a dropdown menu from which you can select the quiz you want to take
+* when the student has successfully finished an exercise, an encoded string is shown
+  that contains the name of the exercise, the mistakes s/he made, and 
+  the time s/he finished the exercise (to be used as proof that the exercise was successfully finished)
 
 ## repo structure
 
 ```
-|- old_versions : contains the old html and jpg files
-|- new_versions : contains the new html files (manually converted) + the old jpg files
-|- new_versions_auto : contains the new html files (converted by the convert.py script)
-|- convert.py: script that converts the old versions into new versions, using a template file
-|- template.html: template for the new versions. 
+|- old_versions/ : contains the old html and jpg files
+|- img/ : contains the map image files
+|- map_data/ : contains a json file for each map, including the path to the map image,
+|              the title of the exercise, and the coordinates of the features to be identified
+|- convert.py: script that converts the old versions into new versions, using a template file (map-quiz.html)
+|- map-quiz.html: the main map quiz file, from which one can take any of the map quizzes 
+|                 (this file also doubles as the template for the new versions of the map quiz files)
+|- file_list.json : a list of all json files in the map_data folder (used to create the dropdown menu)
+|- index.html: the updated homepage of the original mapquiz project, with links to the new html files
 |- placenames.html: all place names in the quizzes
-|- index.html: the original homepage of the mapquiz project, with links to the new files
 ```
+In addition, the root folder contains a html file for each map quiz (cities.html, countries.html, etc.)
--- convert.py
+++ convert.py
@@ -6,23 +6,33 @@ And add a few features.
 import re
 import os
 import shutil
+import json
+
+file_list = ["./map_data/"+ fn for fn in os.listdir("map_data")]
 
 def convert_mapping(map_html):
-    """Convert the <area> tags in the <map> tags into dictionary format"""
+    """Convert the <area> tags in the <map> tags into dictionary format
+
+    example map tag:
+        <area shape="rect" coords="407,188,417,198" href="javascript:mapclick('Agra', 416, 185);">
+
+    example dictionary entry:
+        "Agra": {"id": 1, "shape": "rect", "coords": "407,188,417,198"},
+    """
     def insert_shape(m):
         """Define whether the shape is a polygon or rectangle based on the number of coordinates"""
         if m.group(0).count(",") > 3:
-            return m.group(0) + '", shape: "poly'
+            return  '"shape": "poly", ' + m.group(0)
         else:
-            return m.group(0) + '", shape: "rect'
+            return '"shape": "rect", ' + m.group(0)
 
-    map_str = re.sub('<area shape="(?:poly|rect)" coords=', "coords: ", map_html.strip())
-    map_str = re.sub("""(coords: "[^"]+") href="javascript:mapclick\(('[^']+').+""", 
-                    r"        \2: {id: %d, \1},", map_str)
-    map_str = re.sub("""coords: "([^"]+)""", insert_shape, map_str)
+    map_str = re.sub('<area shape="(?:poly|rect)" coords=', '"coords": ', map_html.strip())
+    map_str = re.sub("""("coords": "[^"]+") href="javascript:mapclick\('([^']+)'.+""", 
+                    r"""        "\2": {"id": %d, \1},""", map_str)
+    map_str = re.sub('''("coords": "[^"]+)"''', insert_shape, map_str)
     map_str = "\n".join([x % (i+1)  for i, x in enumerate(map_str.split("\n"))])
 
-    return map_str
+    return "{\n" + map_str.strip(",") + "\n}"
 
 def convert_file(quiz_name, old_fp, new_fp, template_fn):
     """Convert a single html file"""
@@ -31,6 +41,27 @@ def convert_file(quiz_name, old_fp, new_fp, template_fn):
         html = file.read()
         mapping = "\n".join(re.findall("<area.+", html))
         map_str = convert_mapping(mapping)
+
+    # extract the image file name:
+    image_fn = re.findall('<img src="([^"]+)', html)[0]
+
+    # extract the quiz name from the page's title:
+    quiz_long_name = re.findall('<title>([^<]+)', html)[0]
+    
+    # store the map data:
+    map_data = {
+        "quiz_name": quiz_long_name,
+        "quiz_filename": quiz_name,
+        "map_image": "../img/" + image_fn,
+        "mapping": json.loads(map_str)}
+    data_fp = f"./map_data/{quiz_name}.json"
+    with open(data_fp, mode="w", encoding="utf-8") as file:
+        json.dump(map_data, file, indent=2, ensure_ascii=False)
+    if data_fp not in file_list:
+        file_list.append(file_list)
+    
+    with open("file_list.json", mode="w", encoding="utf-8") as file:
+        json.dump(file_list, file, indent=2, ensure_ascii=False)
 
     # open the template and replace the placeholders with the data:
     with open(template_fn, mode="r", encoding="utf-8") as file:
@@ -42,7 +73,7 @@ def convert_file(quiz_name, old_fp, new_fp, template_fn):
     with open(new_fp, mode="w", encoding="utf-8") as file:
         file.write(new)
 
-def convert_folder(in_folder, out_folder, template_fn):
+def convert_folder(in_folder, out_folder, template_fn, copy_images=False):
     """Convert all html files in a folder and copy the image files"""
     if not os.path.exists(out_folder):
         os.makedirs(out_folder)
@@ -52,19 +83,20 @@ def convert_folder(in_folder, out_folder, template_fn):
             new_fp = os.path.join(out_folder, fn)
             quiz_name = fn[:-5]
             convert_file(quiz_name, old_fp, new_fp, template_fn)
-            old_img_fp = re.sub("\.html", ".jpg", old_fp)
-            new_img_fp = re.sub("\.html", ".jpg", new_fp)
-            shutil.copyfile(old_img_fp, new_img_fp)
+            if copy_images:
+                old_img_fp = re.sub("\.html", ".jpg", old_fp)
+                new_img_fp = re.sub("\.html", ".jpg", new_fp)
+                shutil.copyfile(old_img_fp, new_img_fp)
 
 
 
 if __name__ == "__main__":
 
     quiz_name = "tarim"
-
     old_fp = f"old_versions/{quiz_name}.html"
     new_fp = f"new_versions/{quiz_name}.html"
     template_fn = "template.html"
-    convert_file(quiz_name, old_fp, new_fp, template_fn)
+    template_fn = "map-quiz.html"
+    #convert_file(quiz_name, old_fp, new_fp, template_fn)
 
-    convert_folder("old_versions", "new_versions_auto", template_fn)
+    convert_folder("old_versions", ".", template_fn, copy_images=False)
